2ede33f31a87859305c1f2c6269d477f1365d90d
Shayan Hamidi Dehshali <shayanhamidi2000@gmail.com>
2025-11-28 13:49:29 -0500

    Plotting and Analysis updated for more meaningful results!

--- log_analyzer/analyze.py
+++ log_analyzer/analyze.py
@@ -2,15 +2,39 @@ from pathlib import Path
 from geopy.distance import geodesic
 import sys, os, csv
 import pandas as pd
-import numpy as np
 import plot
 
-SPEED_OF_LIGHT_VAC = 299.792458  # km/ms
+SPEED_OF_LIGHT_VAC = 299792458.0  # m/s
 
-# Command line help
-def printHelp():
-    print("analyze.py --help")
-    print("analyze.py [simulation_summary_file.csv] [ground_stations_file.csv]")
+def get_distance_time_detail(distance_file, max_sim_time):
+    splited_filename = distance_file[:-4].split("#")
+    if len(splited_filename) != 5:
+        raise NameError("Incorrect distance file name format!")
+    if splited_filename[0] != "Distances":
+        raise NameError("Only distances files are accepted, and they start with 'Distances'!")
+    time_step = int(splited_filename[3][:-2])
+    total_time = int(splited_filename[4][:-1]) * 1000
+
+    last_dist_time = (max_sim_time // time_step) * time_step
+    if last_dist_time > total_time:
+        raise ValueError("The provided max simulation time exceeds the total time in the distance file!")
+
+    return last_dist_time, time_step
+
+def extract_distance_time_limit(distance_file, last_dist_time):
+    distances_dict = {}
+    with open(distance_file, "r", newline="") as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip header
+        for row in reader:
+            time_stamp = int(row[0])
+            first_device_id = row[1]
+            second_device_id = row[2]
+            if time_stamp > last_dist_time:
+                break
+            distances_dict[(time_stamp, first_device_id, second_device_id)] = float(row[3])
+        
+    return distances_dict
 
 # Calculate lantnecy
 def packet_latency(group: pd.DataFrame) -> pd.Series:
@@ -21,9 +45,10 @@ def packet_latency(group: pd.DataFrame) -> pd.Series:
     return deliver_times.min() - send_times.min()
 
 def pairwise_latency(sim_df: pd.DataFrame) -> pd.Series:
-    return sim_df.groupby("PacketId").apply(packet_latency, include_groups=False ).dropna().reset_index(name="Latency_ms")
+    return sim_df.groupby("PacketId").apply(packet_latency, include_groups=False).dropna().reset_index(name="Latency_ms")
 
-def add_gs_pairs(sim_df: pd.DataFrame, perf_metrics_df: pd.DataFrame) -> pd.Series:
+def add_gs_pairs(sim_df: pd.DataFrame, perf_metrics_df: pd.DataFrame, gs_df: pd.DataFrame) -> pd.Series:
+    gs_coords = gs_df.set_index(gs_df.columns[0])[["Latitude", "Longitude"]].apply(tuple, axis=1).to_dict()
     send_map = (sim_df[sim_df["Event"] == "DELIVERED"]
                 .sort_values("TimeStamp(ms)")
                 .drop_duplicates("PacketId")
@@ -36,26 +61,21 @@ def add_gs_pairs(sim_df: pd.DataFrame, perf_metrics_df: pd.DataFrame) -> pd.Seri
     
     perf_metrics_df["FromDevice"] = perf_metrics_df["PacketId"].map(send_map)
     perf_metrics_df["ToDevice"] = perf_metrics_df["PacketId"].map(deliver_map)
+    perf_metrics_df["Geodesic"] = perf_metrics_df.apply(lambda row: geodesic(gs_coords.get(row["FromDevice"]), gs_coords.get(row["ToDevice"])).meters, axis=1)
 
 def pairwise_hop_count(sim_df: pd.DataFrame, perf_metrics_df: pd.DataFrame) -> pd.Series:
-    hop_counts = sim_df.query('Event == "RECEIVE"').groupby("PacketId").size().rename("Hop_count")
+    hop_counts = sim_df.query('Event == "RECEIVE"').groupby("PacketId").size().rename("Hop_Count")
     hop_counts = hop_counts.reindex(perf_metrics_df["PacketId"], fill_value=0)
-    perf_metrics_df["Hop_count"] = perf_metrics_df["PacketId"].map(hop_counts)
+    perf_metrics_df["Hop_Count"] = perf_metrics_df["PacketId"].map(hop_counts)
 
-def pairwise_stretch_factor(gs_df: pd.DataFrame, overall: pd.DataFrame) -> pd.Series:
-    gs_coords = gs_df.set_index(gs_df.columns[0])[["Latitude", "Longitude"]].apply(tuple, axis=1).to_dict()
-    def stretch_factor(row):
-        if row["FromDevice"] == "ALL":
-            return pd.NA
-        p1 = gs_coords.get(row["FromDevice"])
-        p2 = gs_coords.get(row["ToDevice"])
-        if p1 is None or p2 is None:
-            return pd.NA
-        distance = geodesic(p1, p2).kilometers
-        light_speed_latency = distance / SPEED_OF_LIGHT_VAC
-        return row["Latency_ms"] / light_speed_latency if light_speed_latency else pd.NA
-    
-    overall["Stretch_factor"] = overall.apply(stretch_factor, axis=1)
+def distance_at_time(group: pd.DataFrame, distances, time_step) -> pd.Series:
+    group["TimeStamp(ms)"] = (group["TimeStamp(ms)"] // time_step) * time_step
+    return group.apply(lambda r: distances.get((r["TimeStamp(ms)"], r["FromDevice"], r["ToDevice"]), pd.NA),axis=1).sum()
+
+def pairwise_stretch_factor(sim_df: pd.DataFrame, distances: dict, perf_metrics_df: pd.DataFrame, time_step: int) -> pd.Series:
+    distances = sim_df.query('Event == "RECEIVE"').groupby("PacketId").apply(lambda g: distance_at_time(g, distances, time_step), include_groups=False).dropna().rename("Distance")
+    distances = distances.reindex(perf_metrics_df["PacketId"], fill_value=0)
+    perf_metrics_df["Stretch_Factor"] = perf_metrics_df["PacketId"].map(distances) / perf_metrics_df["Geodesic"]
 
 #Calculate city pair RTT
 def pairwise_rtt(overall_df: pd.DataFrame) -> None:
@@ -80,13 +100,13 @@ def pairwise_usage(sim_df: pd.DataFrame) -> pd.DataFrame:
 
 #Calculate network jitter
 def pairwise_jitter(perf_df: pd.DataFrame, overall: pd.DataFrame) -> None:
-    jitter_df = (perf_df.groupby(["FromDevice", "ToDevice"])["Latency_ms"].std(ddof=0).rename("jitter").reset_index())
+    jitter_df = (perf_df.groupby(["FromDevice", "ToDevice"])["Latency_ms"].std(ddof=0).rename("Jitter").reset_index())
     return overall.merge(jitter_df, on=["FromDevice", "ToDevice"],how="left", copy=False)
 
 
 def show_results(overall: pd.DataFrame, number_of_dropped_packets, number_of_delivered_packets, throughput) -> None:
     print("Analysis Results:")
-    display_cols = ["FromDevice", "ToDevice", "Latency_ms", "RTT_ms", "Avg_Hop", "Stretch_factor","jitter"]
+    display_cols = ["FromDevice", "ToDevice", "Latency_ms", "RTT_ms", "Hop_Count", "Effective_Latency_Factor", "Jitter", "Total_Packets"]
     print(overall[display_cols].to_string(index=False))
     print("================================================================")
 
@@ -96,53 +116,83 @@ def show_results(overall: pd.DataFrame, number_of_dropped_packets, number_of_del
     print("================================================================")
 
 # Analyze
-def analyze(sim_csv: Path, gs_csv: Path) -> None:
+def analyze(sim_csv: Path, gs_csv: Path, distance_csv: Path) -> None:
     # Load CSV files
     sim_df = pd.read_csv(sim_csv)
     gs_df = pd.read_csv(gs_csv)
 
     # 1. Pair-wise latency
     perf_metrics_df = pairwise_latency(sim_df)
+    print("Pairwise latency calculated.")
 
     # Find source and destination names
-    add_gs_pairs(sim_df, perf_metrics_df)
+    add_gs_pairs(sim_df, perf_metrics_df, gs_df)
 
     # 2. Calculate Hop Counts
     pairwise_hop_count(sim_df, perf_metrics_df)
+    print("Pairwise hop counts calculated.")
+
+    #3. Calculate Stretch Factor
+    max_sim_time = sim_df["TimeStamp(ms)"].max()
+    last_dist_time, time_step = get_distance_time_detail(distance_csv.name, max_sim_time)
+    distances = extract_distance_time_limit(distance_csv, last_dist_time)
+    pairwise_stretch_factor(sim_df, distances, perf_metrics_df, time_step)
+    print("Pairwise stretch factors calculated.")
 
     # 3. Averaging
     # Calculate average latency and hops for each link
     each_link_mean = (perf_metrics_df
                     .groupby(["FromDevice", "ToDevice"], as_index=False)
                     .agg(Latency_ms=("Latency_ms", "mean"),
-                        Avg_Hop=("Hop_count", "mean")))
+                        Hop_Count=("Hop_Count", "mean"),
+                        Stretch_Factor=("Stretch_Factor", "mean"),
+                        Geodesic=("Geodesic", "mean"),
+                        Total_Packets=("Latency_ms", "count")
+                        )
+                    )
     
-    # 4. Calculate average latency and hops for all links
+    # 4. Calculate average latency, hops, and stretch factor for all links
     overall = (each_link_mean.sort_values(["FromDevice", "ToDevice"]).reset_index(drop=True))
+    print("Per link aggregation Calculated.")
 
-    # 5. Calculate Stretch Factor
-    pairwise_stretch_factor(gs_df, overall)
+
+    # 5. Calculate Effective Latency Factor
+    overall["Effective_Latency_Factor"] = (overall["Latency_ms"] * SPEED_OF_LIGHT_VAC) / (1000.0 * overall["Geodesic"] * overall["Stretch_Factor"])
+    overall.drop(columns=["Geodesic"], inplace=True)
+    print("Effective latency factor calculated.")
 
     # 6. Calculate RTT
     pairwise_rtt(overall)
+    print("RTT calculated.")
 
     # 7. Starlink link usage
     satellite_df = pairwise_usage(sim_df)
     satellite_df.to_csv(results_folder / "link_usage.csv", index=False)
+    print("ISL link usage calculated.")
 
     # 8. jitter
     overall = pairwise_jitter(perf_metrics_df, overall)
+    print("Jitter calculated.")
     
     # 9. Calculate average latency and hops for all links
+    overall["Total_Packets"] = overall.pop("Total_Packets")
+    packet_nums = overall["Total_Packets"]
+    total_packets = packet_nums.sum()
     overall_mean = pd.DataFrame({
         "FromDevice": ["ALL"], "ToDevice": ["ALL"],
-        "Latency_ms": [overall["Latency_ms"].mean()],
-        "Avg_Hop": [overall["Avg_Hop"].mean()],
-        "RTT_ms": [overall["RTT_ms"].mean()],
-        "Stretch_factor":  [overall["Stretch_factor"].mean()]})
+        "Latency_ms": [(overall["Latency_ms"] * packet_nums).sum() / total_packets],
+        "Hop_Count": [(overall["Hop_Count"] * packet_nums).sum() / total_packets],
+        "Stretch_Factor": [(overall["Stretch_Factor"] * packet_nums).sum() / total_packets],
+        "Effective_Latency_Factor":  [(overall["Effective_Latency_Factor"] * packet_nums).sum() / total_packets],
+        "RTT_ms": [(overall["RTT_ms"] * packet_nums).sum() / total_packets],
+        "Jitter": [(overall["Jitter"] * packet_nums).sum() / total_packets],
+        "Total_Packets": [total_packets]
+        }
+    )
     
     # 10. Combine all summary into overall
     overall = (pd.concat([overall, overall_mean], ignore_index=True).sort_values(["FromDevice", "ToDevice"]).reset_index(drop=True))
+    print("Mean values calculated.")
 
     # 11. Calculate dropped packets
     dropped_packets = sim_df.loc[sim_df["Event"] == "DROP", "PacketId"]
@@ -151,42 +201,72 @@ def analyze(sim_csv: Path, gs_csv: Path) -> None:
     delivered_set = set(sim_df.loc[sim_df["Event"] == "DELIVERED", "PacketId"])
     duration_s = (sim_df["TimeStamp(ms)"].max() - sim_df["TimeStamp(ms)"].min()) / 1000
     throughput = len(delivered_set) / duration_s
+    print("Dropped Packets and throughput calculated.")
 
     # 13. Print Results
     show_results(overall, len(dropped_packets), len(delivered_set), throughput)
 
     return overall
 
-
+# Command line help
+def printHelp():
+    print("analyze.py --help")
+    print("analyze.py --analyze [simulation_summary_file.csv] [ground_stations_file.csv] [distances_file.csv]")
+    print("analyze.py --combine [name_of_combination] [<overall_folder1> ...] [<overall_name1> ...]")
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Please provide an option or a file name as a command line argument!")
+        print("Please provide an option as a command line argument!")
         printHelp()
         exit(1)
-    
     if sys.argv[1] == "--help":
         printHelp()
         exit(1)
-    elif len(sys.argv) != 3:
+    elif sys.argv[1] == "--analyze" and len(sys.argv) == 5:
+        sim_csv_name = sys.argv[2]
+        gs_csv_name  = sys.argv[3]
+        distance_csv_name = sys.argv[4]
+
+        # Absolute csv file path
+        repo_root = Path(__file__).resolve().parent.parent
+
+        sim_csv_path = repo_root / "generated" / sim_csv_name
+        distance_csv_path = repo_root / "generated" / distance_csv_name
+        gs_csv_path  = repo_root / "configs"  / gs_csv_name
+        results_folder = repo_root / "results" / f'AnalysisOf{sim_csv_name[:-4]}'
+
+        os.makedirs(results_folder, exist_ok=True)
+
+        overall = analyze(sim_csv_path, gs_csv_path, distance_csv_path)
+        overall.to_csv(results_folder / "overall.csv", index=False)
+        
+        plot.generate_plots(overall, results_folder)
+    elif sys.argv[1] == "--combine" and len(sys.argv) >= 3:
+        comb_name = sys.argv[2]
+        file_args = sys.argv[3:]
+        if len(file_args) % 2 != 0:
+            print("Please provide an even number of arguments for combining overall files and their names!")
+            printHelp()
+            exit(1)
+        if len(file_args) < 4:
+            print("At least two overall files are required for combination!")
+            printHelp()
+            exit(1)
+        
+        repo_root = Path(__file__).resolve().parent.parent
+
+        overall_folders = [repo_root / "results" / f for f in file_args[:len(file_args)//2]]
+        overall_names = file_args[len(file_args)//2:]
+        overall_dfs = [pd.read_csv(f / "overall.csv") for f in overall_folders]
+        link_usage_dfs = [pd.read_csv(f / "link_usage.csv") for f in overall_folders]
+        overalls = list(zip(overall_names, overall_dfs, link_usage_dfs))
+
+        combination_folder = repo_root / "results" / f'CombinedAnalysisOf_{comb_name}_{"_".join(overall_names)}'
+
+        os.makedirs(combination_folder, exist_ok=True)
+
+        plot.combine_overalls(overalls, combination_folder, comb_name)
+    else:
         print("Invalid Option or Missing Arguments!")
         printHelp()
         exit(1)
-
-
-    sim_csv_name = sys.argv[1]
-    gs_csv_name  = sys.argv[2]
-
-    # Absolute csv file path
-    repo_root = Path(__file__).resolve().parent.parent
-
-    sim_csv_path = repo_root / "generated" / sim_csv_name
-    gs_csv_path  = repo_root / "configs"  / gs_csv_name
-    results_folder = repo_root / "results" / f'AnalysisOf{sim_csv_name[:-4]}'
-
-    os.makedirs(results_folder, exist_ok=True)
-
-    overall = analyze(sim_csv_path, gs_csv_path)
-    overall.to_csv(results_folder / "overall.csv", index=False)
-    
-    plot.generate_plots(overall, results_folder)
--- log_analyzer/plot.py
+++ log_analyzer/plot.py
@@ -1,30 +1,33 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import random
 
-def hop_count_stretch_factor_cdf_plot(overall: pd.DataFrame,results_dir: str) -> None:
-    def empirical_cdf(arr):
-        # Sort values and compute CDF
-        x = np.sort(arr)
-        y = np.arange(1, len(x)+1) / len(x)
-        return x, y
-    stretch_values = overall.loc[overall["FromDevice"] != "ALL", "Stretch_factor"].dropna().values
-    hop_values = overall["Avg_Hop"].dropna().values
-    sort_x, sort_cdf = empirical_cdf(stretch_values)
-    hop_x, hop_cdf = empirical_cdf(hop_values)
+def empirical_cdf(arr):
+    # Sort values and compute CDF
+    x = np.sort(arr)
+    y = np.arange(1, len(x)+1) / len(x)
+    return x, y 
+
+def hop_count_stretch_factor_effective_latency_cdf_plot(overall: pd.DataFrame,results_dir: str) -> None:
+    stretch_x, stretch_cdf = empirical_cdf(overall.loc[overall["FromDevice"] != "ALL", "Stretch_Factor"].dropna().values)
+    hop_x, hop_cdf = empirical_cdf(overall.loc[overall["FromDevice"] != "ALL", "Hop_Count"].dropna().values)
+    effective_x, effective_cdf = empirical_cdf(overall.loc[overall["FromDevice"] != "ALL", "Effective_Latency_Factor"].dropna().values)
 
     # Draw the CDF plot
     plt.figure()
-    plt.plot(sort_x, sort_cdf, linestyle='-',  label="Stretch Factor CDF")
-    plt.plot(hop_x, hop_cdf, linestyle='--', label="Hop Count CDF")
-    plt.xlabel("Stretch Factor or Hop Count")
-    plt.ylabel("CDF Across Links")
-    plt.title("CDF of Stretch Factor & Hop Count")
+    plt.plot(stretch_x, stretch_cdf, linestyle='-',  label="Stretch Factor CDF", color='blue')
+    plt.plot(hop_x, hop_cdf, linestyle='--', label="Hop Count CDF", color='green')
+    plt.plot(effective_x, effective_cdf, linestyle=':', label="Effective Latency Factor CDF", color='red')
+    plt.xlabel("Factor(Number)")
+    plt.ylabel("CDF")
+    plt.title("CDF of Stretch Factor, Hop Count, and Effective Latency Factor")
     plt.legend()
     plt.grid(True)
 
-    plt.savefig(f"{results_dir}/hop_stretch_cdf_plot.png")
-    print(f'Stretch Factor and Hop Count CDF plot saved as {results_dir}/hop_stretch_cdf_plot.png')
+    plt.savefig(f"{results_dir}/hop_stretch_effective_cdf_plot.png")
+    print(f'Stretch Factor, Hop Count, and Effective Latency Factor CDF plot saved as {results_dir}/hop_stretch_effective_cdf_plot.png')
 
 # Plotting city pair RTT
 def RTT_cdf_plot(overall: pd.DataFrame,results_dir: str) -> None:
@@ -56,13 +59,69 @@ def satellite_usage_plot(results_dir: str) -> None:
     plt.savefig(f"{results_dir}/Link_usage_plot.png")
     print(f'Link usage plot saved as {results_dir}/Link_usage_plot.png')
 
-
-
 def generate_plots(overall: pd.DataFrame, results_dir: str) -> None:
     """
     Generate plots for the analysis results.
     """
     # Plot Hop Count and Stretch Factor CDF
-    hop_count_stretch_factor_cdf_plot(overall,results_dir)
-    RTT_cdf_plot(overall,results_dir)
+    hop_count_stretch_factor_effective_latency_cdf_plot(overall, results_dir)
+    RTT_cdf_plot(overall, results_dir)
     satellite_usage_plot(results_dir)
+
+def combine_hop_count_stretch_factor_effective_latency(overalls, results_dir, combination_name: str) -> None:
+    plt.figure(dpi=300)
+    for overall_name, overall_df, _, _color in overalls:
+        stretch_x, stretch_cdf = empirical_cdf(overall_df.loc[overall_df["FromDevice"] != "ALL", "Stretch_Factor"].dropna().values)
+        hop_x, hop_cdf = empirical_cdf(overall_df.loc[overall_df["FromDevice"] != "ALL", "Hop_Count"].dropna().values)
+        effective_x, effective_cdf = empirical_cdf(overall_df.loc[overall_df["FromDevice"] != "ALL", "Effective_Latency_Factor"].dropna().values)
+        plt.plot(stretch_x, stretch_cdf, linestyle='-',  label=f"Stretch Factor({overall_name})", color=_color, lw=0.8)
+        plt.plot(hop_x, hop_cdf, linestyle='--', label=f"Hop Count({overall_name})", color=_color, lw=0.8)
+        plt.plot(effective_x, effective_cdf, linestyle=':', label=f"Effective Latency Factor({overall_name})", color=_color, lw=0.8)
+
+    plt.xlabel("Factor(Number)")
+    plt.ylabel("CDF")
+    plt.title(f"CDF of Stretch Factor, Hop Count, and Effective Latency Factor for {combination_name}", fontsize=10)
+    plt.legend()
+    plt.grid(True)
+
+    plt.savefig(f"{results_dir}/combined_hop_stretch_effective_cdf_plot.png")
+    print(f'Combined Stretch Factor, Hop Count, and Effective Latency Factor CDF plot saved as {results_dir}/combined_hop_stretch_effective_cdf_plot.png')
+
+def combine_RTT(overalls, results_dir, combination_name: str) -> None:
+    plt.figure(dpi=300)
+    for overall_name, overall_df, _, _color in overalls:
+        rtt_series = pd.to_numeric(overall_df["RTT_ms"], errors="coerce").dropna()
+        draw_rtt = np.sort(rtt_series.values)
+        cdf = np.arange(1, len(draw_rtt) + 1) / len(draw_rtt)
+        plt.plot(draw_rtt, cdf, drawstyle='steps-post', color=_color, label=f'City-RTT CDF({overall_name})', lw=0.8)
+
+    plt.xlabel('City-city RTT (ms)', fontsize=12)
+    plt.ylabel('CDF across city-pairs', fontsize=12)
+    plt.title(f'CDF of City-RTTs for {combination_name}', fontsize=10)
+    plt.legend()
+    plt.grid(True)
+
+    plt.savefig(f"{results_dir}/combined_RTT_cdf_plot.png")
+    print(f'Combined RTT and CDF plot saved as {results_dir}/combined_RTT_cdf_plot.png')
+
+def combine_satellite_usage(overalls, results_dir, combination_name: str) -> None:
+    plt.figure(dpi=300)
+    for overall_name, _, link_usage_df, _color in overalls:
+        plt.plot(link_usage_df["UsageCount"].values,drawstyle="steps-post", color=_color,label=f"Link usage({overall_name})", lw=0.8)
+
+    plt.xlabel("Links ordered by usage")
+    plt.ylabel("No. of packets using a link")
+    plt.title(f'Inter-Satellite Link (ISL) Usage Frequency for {combination_name}', fontsize=10)
+    plt.grid(True)
+    plt.legend()
+
+    plt.savefig(f"{results_dir}/combined_link_usage_plot.png")
+    print(f'Combined Link usage plot saved as {results_dir}/combined_link_usage_plot.png')
+
+def combine_overalls(overalls: list, combination_folder: str, combination_name: str) -> None:
+    colors = list(mcolors.TABLEAU_COLORS.values())
+    random.shuffle(colors)
+    overalls_with_colors = [(overall_name, overall_df, link_usage_df, colors[i % len(colors)]) for i, (overall_name, overall_df, link_usage_df) in enumerate(overalls)]
+    combine_hop_count_stretch_factor_effective_latency(overalls_with_colors, combination_folder, combination_name)
+    combine_RTT(overalls_with_colors, combination_folder, combination_name)
+    combine_satellite_usage(overalls_with_colors, combination_folder, combination_name)
